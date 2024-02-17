5a9c45f77d34002dac12bf6e4bcf02cd69be8f0d
yapyekorebecca <yapyekor@gmail.com>
2024-02-17 15:49:35 +0300

    My Commit

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Guys")
                 }
             }
         }
