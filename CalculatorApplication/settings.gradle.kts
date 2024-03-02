4d2618ce8b9f811993fed5aa24f9320f4c1e73d5
nankyashadia <shadianankya979@gmail.com>
2024-03-02 13:02:59 +0300

    changed setting.gradle merge conflicts

--- CalculatorApplication/settings.gradle.kts
+++ CalculatorApplication/settings.gradle.kts
@@ -12,11 +12,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-
-<<<<<<<< HEAD:CalculatorApplication/settings.gradle.kts
 rootProject.name = "Calculator Application"
-========
-rootProject.name = "Greeting App"
->>>>>>>> 75864eede69b56cfe6bec5af2c4b33871f68bc51:GreetingApp/settings.gradle.kts
 include(":app")
  
