9bb215af2260727b3584876b7dc0bd010fee3644
yapyekorebecca <yapyekor@gmail.com>
2024-03-07 15:04:04 +0300

    added color to the surface container

--- GreetingApp/app/src/main/java/com/example/greetingapp/MainActivity.kt
+++ GreetingApp/app/src/main/java/com/example/greetingapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.greetingapp.ui.theme.GreetingAppTheme
 
@@ -31,16 +32,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color= Color.Magenta) {
+        Text(
+            text = "Hi my name is  $name!",
+            //modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GreetingAppTheme {
-        Greeting("Android")
+        Greeting("Rebecca, Stacey and Shadia")
     }
 }
